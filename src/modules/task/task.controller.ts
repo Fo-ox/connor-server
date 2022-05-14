9fd3590c00553450233c792879e27e3e19518e84
Olga Roganova <olga.roganova@netcracker.com>
2022-05-14 23:18:30 +0300

    CON-00004 - add task controller

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,12 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TaskDto } from './dto/task.dto';
 
 @Controller('task')
 export class TaskController {
 
+    private JIRA_AUTOMATION;
+
     @Post('/create')
     createTask(@Body() newTask: TaskDto): any {
-        console.log(newTask);
+        this.JIRA_AUTOMATION = newTask;
         return newTask;
+    }
+
+    @Get()
+    getTasks(): any {
+        return this.JIRA_AUTOMATION
     }
 }
