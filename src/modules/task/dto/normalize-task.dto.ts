773df993a704c1a06c1c89daff1efb433c960f5e
Olga Roganova <olga.roganova@netcracker.com>
2022-05-15 23:51:33 +0300

    CON-00004 - add task documentation

--- src/modules/task/dto/normalize-task.dto.ts
+++ src/modules/task/dto/normalize-task.dto.ts
@@ -1,22 +1,11 @@
-export class TaskDto {
+export class NormalizeTaskDto {
     id: string;
-    internalSystemId?: string;
-    name: string;
-    description?: string;
-    projectId: string;
-    statusId: string;
-    completed?: boolean;
     typeId: string;
     priorityId?: string;
     tags?: string; // unzip enumerate types
-    reporterId?: string;
     assigneeId?: string;
+    assigneeHardSkillsLevel?: string;
     createDate: string;
-    closeDate?: string;
     initialEstimate?: string;
-    resolvedEstimate?: number;
-    predictEstimate?: number;
-    predictorVersion?: string;
-    predictorType?: string;
 }
 
